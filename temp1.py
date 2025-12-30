3c847e2ca7d7b4613d73ce331ee145e6df2dbf16
Avazbek Ernazarov <avazbekernazarov80@gmail.com>
2025-12-30 16:25:36 +0500

    cod-new

---
+++ temp1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def a(komp, vodel, chiqdi):
+    av={'komp':komp,
+       'vodel':vodel,
+       'chiqdi':chiqdi}
+    return a
+print(a)
+
+
+
